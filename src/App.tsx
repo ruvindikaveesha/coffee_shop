717e9cd1804a622811560e143dc7592f3f754852
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-12-26 11:22:24 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from "./views/navbar";
 import Header from "./components/layout/header";
 import Footer from "./components/layout/footer";
 import Home from "./views/home";
