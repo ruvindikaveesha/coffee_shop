f3596c7e4aee6cf2cc55da63e8695282b0ac4144
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-12-27 16:24:37 +0530

    update

---
+++ src/components/card/card.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Button from "../card/Button";
+import { BsStarHalf } from "react-icons/bs";
+import { BsStarFill } from "react-icons/bs";
+
+const Card = (props) => {
+    return (
+        <div className=" w-full lg:w-1/4 bg-white p-3 rounded-lg">
+            <div className="flex flex-wrap justify-center">
+                <div className="w-3/12 sm:w-4/12 px-1">
+                    <img src={props.img}
+                         alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none"/>
+                </div>
+            </div>
+            <div className=" flex flex-col items-center mt-5 gap-3">
+                <h2 className=" font-semibold text-xl">{props.title}</h2>
+                <div className=" flex ">
+                    <BsStarFill className=" text-brightColor" />
+                    <BsStarFill className=" text-brightColor" />
+                    <BsStarFill className=" text-brightColor" />
+                    <BsStarFill className=" text-brightColor" />
+                    <BsStarHalf className=" text-brightColor" />
+                </div>
+                <h3 className=" font-semibold text-lg">$15.99</h3>
+                <Button title="Add To Card" />
+            </div>
+        </div>
+    );
+};
+
+export default Card;
---
+++ src/views/review.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReviewCard from "../components/card/reviewcard";
+import img1 from "/src/assets/pic1.png";
+import img2 from "/src/assets/pic2.png";
+import img3 from "/src/assets/pic1.png";
+
+const Review = () => {
+    return (
+        <div className=" min-h-screen flex flex-col justify-center lg:px-32 px-5 bg-orange-100  ">
+            <h1 className=" font-semibold text-center text-4xl lg:mt-14 mt-24 ">
+                Customer's Reviews
+            </h1>
+
+            <div className=" flex flex-col lg:flex-row gap-5 justify-center py-4 my-8">
+                <ReviewCard img={img1} title="Olivia Ava" />
+                <ReviewCard img={img2} title="John Deo" />
+                <ReviewCard img={img3} title="Sofia Zoe" />
+            </div>
+        </div>
+    );
+};
+
+export default Review;
