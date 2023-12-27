58ec57a6b99d41ff8015addae3cd7f47e538bf1b
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-12-27 20:45:51 +0530

    update

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -7,15 +7,14 @@ class Header extends React.Component<any, any> {
   render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | Iterable<React.ReactNode> | React.ReactPortal | boolean | any | null | undefined {
     return(
       <header>
-        <nav className={'flex justify-between items-center bg-white-100 p-5'}>
+        <nav className={'flex justify-between items-center bg-white-100 p-5 font-bold text-amber-950 italic'}>
           <h2 className={'w-[50px] text-2xl font-bold text-amber-950 italic'}>Coffee</h2>
           <ul className={'flex gap-[4vw] font-bold text-amber-950'}>
             <li><Link to={'/'}>HOME</Link></li>
             <li><Link to={'/AboutPage'}>ABOUT US</Link></li>
             <li><Link to={'/menu'}>MENU</Link></li>
-            <li><Link to={'/review'}>REVIEW</Link></li>
-            <li><Link to={'/signin'} className={'"border-solid'} >LOGIN</Link></li>
-
+            <li><Link to={'/review'} >REVIEW</Link></li>
+            <li className={'border-yellow-950'}><Link to={'/signin'} >LOGIN</Link></li>
           </ul>
         </nav>
       </header>
