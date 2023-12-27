fdf36a73e464e3c2146ba4d4c6ad541d8f460d29
ruvindikaveesha <117073231+ruvindikaveesha@users.noreply.github.com>
2023-12-27 19:48:45 +0530

    update

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -13,15 +13,15 @@ const Card = (props) => {
                 </div>
             </div>
             <div className=" flex flex-col items-center mt-5 gap-3">
-                <h2 className=" font-semibold text-xl">{props.title}</h2>
-                <div className=" flex ">
+                <h2 className=" font-semibold text-xl font-bold text-amber-950 italic">{props.title}</h2>
+                <div className=" flex  text-amber-950 ">
                     <BsStarFill className=" text-brightColor" />
                     <BsStarFill className=" text-brightColor" />
                     <BsStarFill className=" text-brightColor" />
                     <BsStarFill className=" text-brightColor" />
                     <BsStarHalf className=" text-brightColor" />
                 </div>
-                <h3 className=" font-semibold text-lg" >{props.price}</h3>
+                <h3 className=" font-semibold text-lg font-bold text-amber-950 italic" >{props.price}</h3>
                 {/*<h3 className=" font-semibold text-lg" >$15.99</h3>*/}
                 <Button title="Order Now" />
             </div>
--- src/components/card/reviewcard.tsx
+++ src/components/card/reviewcard.tsx
@@ -5,13 +5,13 @@ import { BsStarFill } from "react-icons/bs";
 
 const ReviewCard = (props) => {
     return (
-        <div className=" flex flex-col w-full lg:w-2/6 bg-white p-3 rounded-lg gap-5">
+        <div className=" flex flex-col w-full lg:w-2/6 bg-white p-3 rounded-lg gap-5 font-bold text-amber-950 italic">
             <div className=" flex flex-row items-center lg:justify-start justify-center">
                 <div className="w-1/4">
                     <img className=" rounded-full" src={props.img} alt="img" />
                 </div>
                 <div className=" mx-3">
-                    <h2 className=" font-semibold text-lg">{props.title}</h2>
+                    <h2 className=" font-semibold text-lg font-bold text-amber-950 italic">{props.title}</h2>
                     <div className=" flex">
                         <BsStarFill className=" text-brightColor" />
                         <BsStarFill className=" text-brightColor" />
@@ -24,7 +24,7 @@ const ReviewCard = (props) => {
           <FaQuoteRight className=" text-backgroundColor" size={42} />
         </span>
             </div>
-            <p>
+            <p className={'font-bold text-amber-950 italic'}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
                 consequatur fuga suscipit perspiciatis totam. Eos esse nisi omnis dicta,
                 aperiam distinctio fuga ipsam rerum rem.
